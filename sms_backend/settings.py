6505d12f0ba55e5d487bb31acfe2ac04b85869b2
Hck-ney <163156306+Hck-ney@users.noreply.github.com>
2025-06-18 08:18:41 +0800

    Update allowed corsheaders settings.py

--- sms_backend/settings.py
+++ sms_backend/settings.py
@@ -149,6 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000", # For local development if backend is on 8000
     "http://localhost:8000", # For local development if backend is on localhost:8000
+    "https://itec-106-final.onrender.com",
 ]
 
 if 'RENDER' in os.environ:
